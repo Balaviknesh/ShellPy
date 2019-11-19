bd249b24735683b367f15efc4b8113b3a59cc67f
Bala <bala21karthick@gmail.com>
2019-11-18 17:31:00 -0700

    checking

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def get_all_file_paths(directory):
 class ShellPy(Cmd):
 
     promt = " (: "+ getpass.getuser() + " :) > "
-    intro = "ShellPy© Version 0.3"
+    intro = "ShellPy© Version 0.4"
 
 
     def do_cwd(self, inp):
